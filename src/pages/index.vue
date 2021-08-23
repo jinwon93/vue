3b41b7f114bae25de0024893d6d3f4aa65fcf5c5
jinwon93 <kjw8208@nate.com>
2021-08-23 23:11:51 +0900

    toast-pj

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1 class="mt-4">Vue3 기초 익히기</h1>
+        
+</template>
+
+<script>
+import {useCount} from '@/composables/count';
+
+export default {
+    setup(){
+        const {count} =useCount();
+        return{
+            count
+        }
+    }    
+}
+</script>
+
+
+<style>
+
+</style>
