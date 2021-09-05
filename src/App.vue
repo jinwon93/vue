f673231c2f1c03b34799ad12654026726c42c38e
jinwon93 <kjw8208@nate.com>
2021-09-05 21:41:34 +0900

    Delete

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,23 @@
+  
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/vuex">vuex</router-link>
+  <Navbar />
+  <div class="container">
+      <router-view/>
   </div>
-  <router-view/>
-  <Footer />
+  
+  <Toast />
 </template>
 
 <script>
-
-import Footer from './components/common/Footer.vue'; 
-
+import Toast from '@/components/Toast.vue';
+import Navbar from '@/components/Navbar.vue';
 export default {
-  name:'nav',
-  components:{
-    Footer
-  }
+  components: {
+    Toast,
+    Navbar
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
 </style>
